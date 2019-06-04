b1b4e91467c7fae5c234d66ebe2fce4db859a172
Kroos-x <1197798263@qq.com>
2019-06-04 21:48:56 +0800

    日常提交

---
+++ src/main/java/com/lh/system/mapper/RoleMemberMapper.java
@@ -0,0 +1,17 @@
+package com.lh.system.mapper;
+
+import com.lh.system.model.RoleMember;
+
+public interface RoleMemberMapper {
+    int deleteByPrimaryKey(String id);
+
+    int insert(RoleMember record);
+
+    int insertSelective(RoleMember record);
+
+    RoleMember selectByPrimaryKey(String id);
+
+    int updateByPrimaryKeySelective(RoleMember record);
+
+    int updateByPrimaryKey(RoleMember record);
+}
---
+++ src/main/java/com/lh/system/model/Role.java
@@ -0,0 +1,85 @@
+package com.lh.system.model;
+
+import java.util.Date;
+
+public class Role {
+    private String id;
+
+    private Date createDate;
+
+    private String createId;
+
+    private String createName;
+
+    private String name;
+
+    private String remark;
+
+    private Integer sort;
+
+    private Integer state;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id == null ? null : id.trim();
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getCreateId() {
+        return createId;
+    }
+
+    public void setCreateId(String createId) {
+        this.createId = createId == null ? null : createId.trim();
+    }
+
+    public String getCreateName() {
+        return createName;
+    }
+
+    public void setCreateName(String createName) {
+        this.createName = createName == null ? null : createName.trim();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark == null ? null : remark.trim();
+    }
+
+    public Integer getSort() {
+        return sort;
+    }
+
+    public void setSort(Integer sort) {
+        this.sort = sort;
+    }
+
+    public Integer getState() {
+        return state;
+    }
+
+    public void setState(Integer state) {
+        this.state = state;
+    }
+}
