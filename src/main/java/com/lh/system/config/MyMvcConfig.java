61d2dc30242e0028d5447669f98d7ea547e6009c
Kroos-x <1197798263@qq.com>
2019-06-12 23:22:35 +0800

    注册功能

--- src/main/java/com/lh/system/config/MyMvcConfig.java
+++ src/main/java/com/lh/system/config/MyMvcConfig.java
@@ -1,5 +1,6 @@
 package com.lh.system.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -20,20 +21,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 public class MyMvcConfig extends WebMvcConfigurerAdapter {
 
-    // @Bean
+    @Bean
     public WebMvcConfigurerAdapter webMvcConfigurerAdapter() {
         WebMvcConfigurerAdapter adapter = new WebMvcConfigurerAdapter() {
-            //登录拦截器
-            @Override
-            public void addInterceptors(InterceptorRegistry registry) {
-                registry.addInterceptor(new LoginHandlerInterceptor()).addPathPatterns("/**")
-                        .excludePathPatterns("/index.html","/","/user/login");
-            }
+            // //登录拦截器
+            // @Override
+            // public void addInterceptors(InterceptorRegistry registry) {
+            //     registry.addInterceptor(new LoginHandlerInterceptor()).addPathPatterns("/**")
+            //             .excludePathPatterns("/index.html","/","/user/login");
+            // }
             //视图控制器
             @Override
             public void addViewControllers(ViewControllerRegistry registry) {
-                registry.addViewController("/").setViewName("index/login");
-                registry.addViewController("/index.html").setViewName("index/login");
+                registry.addViewController("/").setViewName("index");
+                registry.addViewController("/index.html").setViewName("index");
                 //登录成功后重定向到首页
                 registry.addViewController("/main.html").setViewName("dashboard");
             }
