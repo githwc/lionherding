5d9ef790b853ab88c9edc3c06f62cbbaa2d49f9c
Kroos-x <1197798263@qq.com>
2019-06-28 19:03:47 +0800

    继承mybatis-plus改版升级其他配置

--- src/main/java/com/lh/modules/Test/controller/TestController.java
+++ src/main/java/com/lh/modules/Test/controller/TestController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/test")
-public class TestAction  {
+public class TestController {
 
     @Autowired
     private TestService testService;
